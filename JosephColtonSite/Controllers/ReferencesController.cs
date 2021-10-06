f40ef761468535f83dfcb756269d11080c07b214
Joseph Colton <josephcolton@gmail.com>
2021-10-06 10:55:30 -0700

    Added lab 2 stuff

---
+++ JosephColtonSite/Controllers/ReferencesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JosephColtonSite.Controllers
+{
+    public class ReferencesController : Controller
+    {
+        public IActionResult References()
+        {
+            return View();
+        }
+
+        public IActionResult Books()
+        {
+            return View();
+        }
+
+    }
+}
